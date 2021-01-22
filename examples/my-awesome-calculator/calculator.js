45091b9eb70329e1c718183637b2333fb2c32514
Patsy Issa <patsy.issa@gmail.com>
2021-01-22 20:23:19 +0100

    wip

--- examples/my-awesome-calculator/calculator.js
+++ examples/my-awesome-calculator/calculator.js
@@ -4,7 +4,6 @@ const n1 = prompt("Enter first number: ");
 const n2 = prompt("Enter second number: ");
 
 let addition = Number(n1) + Number(n2);
-
-// let multiplication = Number(n1) + Number(n2)
+let multiplication = Number(n1) + Number(n2);
 
 console.log(`Your result is: ${result}`);
