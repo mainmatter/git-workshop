5a1a8063073bd0fcb3e875bfe86489969cb7b3d6
Patsy Issa <patsy.issa@gmail.com>
2021-01-22 20:18:51 +0100

    wip multiplication

--- examples/my-awesome-calculator/calculator.js
+++ examples/my-awesome-calculator/calculator.js
@@ -3,6 +3,8 @@ const prompt = require("prompt-sync")({ sigint: true });
 const n1 = prompt("Enter first number: ");
 const n2 = prompt("Enter second number: ");
 
-let result = Number(n1) + Number(n2);
+let addition = Number(n1) + Number(n2);
+
+// let multiplication = Number(n1) + Number(n2)
 
 console.log(`Your result is: ${result}`);
