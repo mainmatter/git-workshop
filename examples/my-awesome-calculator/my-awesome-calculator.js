9bad222371f248764bf7474252d7245d7bd5fcc6
l_wydra <l.wydra@mvb-online.de>
2021-02-26 14:46:52 +0100

    implement multiplication logic

--- examples/my-awesome-calculator/my-awesome-calculator.js
+++ examples/my-awesome-calculator/my-awesome-calculator.js
@@ -1,6 +1,7 @@
 export default function awesomeCalculator(n1, n2) {
   let addition = n1 + n2;
   let division = n1 / n2;
+  let multiplication = n1 * n2;
 
-  return { addition, division };
+  return { addition, division, multiplication };
 }
